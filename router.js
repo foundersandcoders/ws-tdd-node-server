2f8648fa52353af16aeb6ec1cd391e60485f8733
edificex <edificex@posteo.net>
2020-03-22 18:08:16 +0000

    make example supertest "text/plain" in both test and router files, and make text "hello" in solution, as per the README

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
 const router = (req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Hello");
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'text/plain' })
+    res.end('hello')
   }
-};
+}
 
-module.exports = router;
+module.exports = router
--- test.js
+++ test.js
@@ -1,21 +1,21 @@
-const test = require("tape");
-const supertest = require("supertest");
-const router = require("./router");
+const test = require('tape')
+const supertest = require('supertest')
+const router = require('./router')
 
-test("Initialise", t => {
-  let num = 2;
-  t.equal(num, 2, "Should return 2");
-  t.end();
-});
+test('Initialise', t => {
+  let num = 2
+  t.equal(num, 2, 'Should return 2')
+  t.end()
+})
 
-test("Home route", t => {
+test('Home route', t => {
   supertest(router)
-    .get("/")
+    .get('/')
     .expect(200)
-    .expect("Content-Type", "text/plain")
+    .expect('Content-Type', 'text/plain')
     .end((err, res) => {
-      t.error(err);
-      t.equal(res.text, "Hello");
-      t.end();
-    });
-});
+      t.error(err)
+      t.equal(res.text, 'hello')
+      t.end()
+    })
+})
